3fa68d376e1d6d9ae79b407b10bb040d016d0054
khongyeuai <67258104+khongyeuai@users.noreply.github.com>
2020-06-22 23:01:03 +0900

    Create snake.h

---
+++ snake.h
@@ -0,0 +1,48 @@
+#include <Windows.h>
+#include <vector>
+
+#pragma once
+
+#define HEIGHT 20
+#define WIDTH 40
+#define X0 18
+#define Y0 10
+#define CHAR 219
+
+struct ToaDo {
+    int x0;
+    int y0;
+    int x;
+    int y;
+};
+
+enum STYLE { LEFT = 0, RIGHT, UP, DOWN };
+
+class snake
+{
+private:
+    ToaDo head;
+    std::vector<ToaDo>  tail;
+    bool gameOver;
+    int prev;
+    unsigned int score;
+    ToaDo tao;
+    int soDuoi;
+    int speed ;
+public:
+    snake();
+    ~snake();
+    void veKhung();
+    void veRan();
+    void dieuKhien();
+    void xoaDiem(int x, int y);
+    void kiemTraThua();
+    void veTao();
+    void veToHon();
+    void veDiem();
+    void kiemTraAn();
+    void initPrev(ToaDo& td);
+    void changeSpeed();
+    unsigned int getScore();
+};
+
